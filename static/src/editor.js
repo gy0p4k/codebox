ad43c73fd03295b32f2fe1f8d9d222d1a90d0231
dobori <dobori@gmail.com>
2018-01-11 10:52:29 +0100

    feat(codebox): custom scrollbar for editor

--- static/src/editor.js
+++ static/src/editor.js
@@ -5,6 +5,7 @@ var editableCodeMirror = CodeMirror.fromTextArea(document.querySelector(".code")
     mode: "javascript",
     theme: "monokai",
     lineNumbers: true,
+    scrollbarStyle: "overlay",
     extraKeys: {
         "F11": function(cm) {
             cm.setOption("fullScreen", !cm.getOption("fullScreen"));
@@ -15,11 +16,9 @@ var editableCodeMirror = CodeMirror.fromTextArea(document.querySelector(".code")
     }
 })
 
-
 let resize = function () {
      // editableCodeMirror.setSize(getProperty(".code", "width"), getProperty(".top", "height"))
-     editableCodeMirror.setSize(document.width, 790)
-     
+     editableCodeMirror.setSize(document.width, document.height)
 }
 
 let updateCode =function(content){
